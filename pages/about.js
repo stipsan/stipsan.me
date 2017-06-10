d2f7fa8df838399022f4091db76c5b9208f1a260
Stian Didriksen <stipsan@gmail.com>
2017-06-11 01:52:08 +0200

    Finished Learning Next.js Lesson #3 https://learnnextjs.com/basics/using-shared-components

--- pages/about.js
+++ pages/about.js
@@ -1,4 +1,6 @@
+import Layout from '../components/MyLayout.js'
+
 export default () =>
-  <div>
+  <Layout>
     <p>This is the about page</p>
-  </div>
+  </Layout>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
-import Link from 'next/link'
+import Layout from '../components/MyLayout.js'
 
-const Index = () =>
-  <div>
-    <Link href="/about">
-      <button>Go to About Page</button>
-    </Link>
-    <p>Hello Next.js!</p>
-  </div>
-
-export default Index
+export default () =>
+  <Layout>
+    <p>Hello Next.js</p>
+  </Layout>
