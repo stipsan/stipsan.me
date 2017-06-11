7b39fd8be39ed3cdf5ca0053b16326998ee3daba
Stian Didriksen <stipsan@gmail.com>
2017-06-11 18:45:56 +0200

    almost happy with menu links, they look less shitty now

--- components/MenuLink.js
+++ components/MenuLink.js
@@ -4,12 +4,14 @@ import Router from 'next/router'
 
 const Link = styled.a`
   cursor: pointer;
-  color: ${props => (props.isActive ? 'red' : 'black')};
+  color: ${props => (props.isActive ? 'white' : '#0f0f0f')};
+  background-color: ${props => (props.isActive ? '#0f0f0f' : 'transparent')};
   display: inline-block;
   margin-left: 24px;
   padding: 3px 8px 4px 8px;
   text-transform: uppercase;
   border-radius: 4px;
+  text-decoration: none;
 `
 
 export default class MenuLink extends Component {
